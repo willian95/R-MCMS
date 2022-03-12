be6ba7b146a6ac68325d208d70ee8675b67bb3d4
willian rodriguez <rodriguezwillian95@gmail.com>
2022-03-11 20:08:06 -0400

    cambios

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,30 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
+use App\Models\Brand;
 use App\Models\ServiceImage;
 
 class ReviewController extends Controller
 {
     public function review()
     {
-        /*$products = Product::all();
+        $brands = Brand::all();
 
-        foreach ($products as $product) {
-            $productModel = Product::where('id', $product->id)->first();
-            $productModel->image = str_replace('https://adminrmvet2.sytes.net', 'https://cms.rymveterinaria.com', $product->image);
-            $productModel->image_hover = str_replace('https://adminrmvet2.sytes.net', 'https://cms.rymveterinaria.com', $product->image_hover);
+        foreach ($brands as $brand) {
+            $productModel = Brand::where('id', $brand->id)->first();
+            $productModel->image = str_replace('https://adminrmvet2.sytes.net', 'https://cms.rymveterinaria.com', $brand->image);
             $productModel->update();
-        }*/
+        }
 
-        $banners = ServiceImage::all();
+        /*$banners = Bran::all();
 
         foreach ($banners as $product) {
             $productModel = ServiceImage::where('id', $product->id)->first();
-            $productModel->image1 = str_replace('https://adminrmvet2.sytes.net', 'https://cms.rymveterinaria.com', $product->image1);
-            $productModel->image2 = str_replace('https://adminrmvet2.sytes.net', 'https://cms.rymveterinaria.com', $product->image2);
-            $productModel->image3 = str_replace('https://adminrmvet2.sytes.net', 'https://cms.rymveterinaria.com', $product->image3);
+            $productModel->image1 = str_replace('adminrmvet2.sytes.net', 'https://cms.rymveterinaria.com', $product->image1);
+            $productModel->image2 = str_replace('adminrmvet2.sytes.net', 'https://cms.rymveterinaria.com', $product->image2);
+            $productModel->image3 = str_replace('adminrmvet2.sytes.net', 'https://cms.rymveterinaria.com', $product->image3);
             $productModel->update();
-        }
+        }*/
     }
 }
